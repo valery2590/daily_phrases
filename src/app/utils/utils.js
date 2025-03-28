e3e27eef93d327fbe2e306416b8155af3a735dd5
valery2590 <valeryfigueroah@gmail.com>
2025-03-28 13:48:14 +0000

    new values added

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -30,6 +30,28 @@ const phrasesList = [
     explanation_en: "",
     explanation_es: "",
   },
+  {
+    author_es: "Seneca",
+    author_en: "Seneca",
+    phrase_en:
+      "Life without a design is erratic. As soon as one is in place, principles become necessary. I think you'll concede that nothing is more shameful than uncertain and weavering conduct, and beating a cowardly retreat. This will happen in all our affairs unless we remove the faults that seize and detain our spirits, preventing them from pushing forward and making an all-out effort",
+    phrase_es:
+      "La vida sin un diseño es errática. Tan pronto como se establece uno, los principios se vuelven necesarios. Creo que admitirás que no hay nada más vergonzoso que una conducta incierta y vacilante y una retirada cobarde. Esto sucederá en todos nuestros asuntos a menos que eliminemos las faltas que se apoderan y detienen nuestro espíritu, impidiéndole seguir adelante y hacer un esfuerzo total.",
+    explanation_en:
+      "Don't stop, don't settle, don't consider it enough. Life is full of new challenges, full of falls and rises. Do not be afraid of the word failure, because it is part of life, and as such, it has its moment. Success has the same way, which comes after this. ",
+    explanation_es:
+      "No te detengas, no te acomodes, no consideres que es suficiente. La vida está llena de nuevos desafíos, llena de caídas y subidas. No le tengas miedo a la palabra fracaso, pues es parte de la vida, y como tal, tiene su momento. Del mismo modo lo tiene el éxito, que viene después de este. ",
+  },
+  {
+    author_es: "Epicteto ",
+    author_en: "Epictetus",
+    phrase_en:
+      "If you should ever turn your will to things outside your control in order to impress someone, be sure that you have wrecked your whole purpose in life. Be content, then, to be a philosopher in all that you do, and if you wish also to be seen as one, show yourself first that you are and you will succeed. ",
+    phrase_es:
+      "Si alguna vez diriges tu voluntad hacia cosas fuera de tu control para impresionar a alguien, asegúrate de haber arruinado todo tu propósito en la vida. Conténtate, pues, con ser filósofo en todo lo que hagas, y si también quieres que te vean como tal, muéstrate primero que lo eres y lo conseguirás.",
+    explanation_en: "",
+    explanation_es: "",
+  },
 ];
 
 export { phrasesList };
